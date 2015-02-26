31d8cb45145e5bce44b49659c5f5fd911f4240c8
frankhuster <fhuster@grameenfoundation.org>
2015-02-26 14:25:43 -0800

    for pawel to fix :-)
    Change-Id: I1f6204206d046ccdcb67afb77127fc21c58d8fea

---
+++ csd/src/main/java/org/motechproject/csd/service/FooBar.java
@@ -0,0 +1,20 @@
+package org.motechproject.csd.service;
+
+import ihe.iti.csd._2013.GetModificationsRequest;
+import ihe.iti.csd._2013.GetModificationsResponse;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+
+@Endpoint
+public class FooBar {
+    @PayloadRoot(namespace = "http://localhost:8080/modules/csd/foobar", localPart = "getFacilityRequest")
+    @ResponsePayload
+    public GetModificationsResponse getFacility(@RequestPayload GetModificationsRequest request) {
+        GetModificationsResponse response = new GetModificationsResponse();
+
+        return response;
+    }
+}
