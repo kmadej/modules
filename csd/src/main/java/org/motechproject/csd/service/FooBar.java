10a42dfa59124c09a6846cf5cfbd583130c3fbf3
frankhuster <fhuster@grameenfoundation.org>
2015-03-03 15:32:38 -0800

    SOAP 1.2 almost, but not quite working
    Change-Id: Ib2b9d19d3dbb59fa2dd3387cf99b5ef9c11636f4

--- csd/src/main/java/org/motechproject/csd/service/FooBar.java
+++ csd/src/main/java/org/motechproject/csd/service/FooBar.java
@@ -2,14 +2,25 @@ package org.motechproject.csd.service;
 
 import ihe.iti.csd._2013.GetModificationsRequest;
 import ihe.iti.csd._2013.GetModificationsResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.ws.soap.SoapVersion;
+import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
 
 
 @Endpoint
 public class FooBar {
+    private SaajSoapMessageFactory messageFactory;
+
+    @Autowired
+    public FooBar(SaajSoapMessageFactory messageFactory) {
+        this.messageFactory = messageFactory;
+        this.messageFactory.setSoapVersion(SoapVersion.SOAP_12);
+    }
+
     @PayloadRoot(namespace = "http://localhost:8080/motech-platform-server/module/csd/foobar", localPart = "getFacilityRequest")
     @ResponsePayload
     public GetModificationsResponse getFacility(@RequestPayload GetModificationsRequest request) {
