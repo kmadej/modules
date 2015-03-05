234c26996f39a6d4d027a30461a0962cb4c178c1
frankhuster <fhuster@grameenfoundation.org>
2015-03-05 14:50:42 -0800

    CSD POJOs auto-generated in build
    Change-Id: I6d9c66f94e56729961fa1e2243cba798dd8a1f5a

--- csd/src/main/java/org/motechproject/csd/service/FooBar.java
+++ csd/src/main/java/org/motechproject/csd/service/FooBar.java
@@ -1,21 +1,53 @@
 package org.motechproject.csd.service;
 
+import ihe.iti.csd._2013.CSD;
+import ihe.iti.csd._2013.Facility;
 import ihe.iti.csd._2013.GetModificationsRequest;
 import ihe.iti.csd._2013.GetModificationsResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.ws.soap.addressing.server.annotation.Action;
 
 
 @Endpoint
 public class FooBar {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FooBar.class);
+
     @PayloadRoot(namespace = "urn:ihe:iti:csd:2013", localPart = "getModificationsRequest")
     @ResponsePayload
-    public GetModificationsResponse getFacility(@RequestPayload GetModificationsRequest request) {
+    public GetModificationsResponse getMod1(@RequestPayload GetModificationsRequest request) {
+        LOGGER.debug("getMod1");
         GetModificationsResponse response = new GetModificationsResponse();
 
+        Facility facility = new Facility();
+        facility.setPrimaryName("Clinique Foobar");
+        CSD.FacilityDirectory facilityDirectory = new CSD.FacilityDirectory();
+        facilityDirectory.getFacility().add(facility);
+        CSD csd = new CSD();
+        csd.setFacilityDirectory(facilityDirectory);
+
+        response.setCSD(csd);
+        return response;
+    }
+
+    @Action(value = "urn:ihe:iti:csd:2013:GetDirectoryModificationsRequest")
+    public GetModificationsResponse getMod2(@RequestPayload GetModificationsRequest request) {
+        LOGGER.debug("getMod2");
+        GetModificationsResponse response = new GetModificationsResponse();
+
+        Facility facility = new Facility();
+        facility.setPrimaryName("Clinique Foobar");
+        CSD.FacilityDirectory facilityDirectory = new CSD.FacilityDirectory();
+        facilityDirectory.getFacility().add(facility);
+        CSD csd = new CSD();
+        csd.setFacilityDirectory(facilityDirectory);
+
+        response.setCSD(csd);
         return response;
     }
 }
